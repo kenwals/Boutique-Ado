4c1f01f791e450ae7d33279902caa360d7a2801e
kenwals <kenwals@gmail.com>
2021-07-11 10:17:07 +0000

    refactor base css and mobile display

--- env-sample.py
+++ env-sample.py
@@ -2,3 +2,10 @@ import os
 
 os.environ.setdefault("STRIPE_PUBLIC_KEY", "secret")
 os.environ.setdefault("STRIPE_SECRET_KEY", "secret")
+os.environ.setdefault("STRIPE_WH_SECRET", "secret")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "secret")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "secret")
+os.environ.setdefault("DATABASE_URL", "secret")
+os.environ.setdefault("EMAIL_HOST_PASS", "secret")
+os.environ.setdefault("EMAIL_HOST_USER", "secret")
+os.environ.setdefault("USE_AWS", "secret")
